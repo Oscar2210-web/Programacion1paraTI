b0c468d5b9363769375785c4bd68cdbff21166d6
Oscar <Oscar@LAPTOP-A9FOF9UE>
2021-06-04 19:49:43 -0600

    ejemplos de metodos

---
+++ src/semana3/Metodos.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana3;
+
+/**
+ *
+ * @author Oscar
+ */
+public class Metodos {
+    public static void main(Straing[] args)
+            
+        }
+
+    public static int pedro(int numero1, int numero2){
+        int resultado;
+        resultado = numero1 + numero2;
+        return resultado;
+        
+        }
+    
+
+    public static int maria(int numero1, int numero2){
+        int resultado;
+        resultado = numero1 * numero2;
+        return resultado;     
+
+        }
+}
